bc1e3efe738cdfb8d322b18a737a11535cd850e7
Deák Ruben <rubendeak15@gmail.com>
2021-12-27 20:47:09 +0200

    Add project files.

---
+++ CarMechanic_Common/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMechanic_Common.Models
+{
+    public enum CarWorkStatus
+    { 
+        Recorded,
+        InProgress,
+        Finished
+    }
+    public class Client
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CarModel { get; set; }
+        public string CarLicencePlateNumber { get; set; }
+        public string CarProblemDescription { get; set; }
+        public CarWorkStatus WorkStatus { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {CarModel} ({CarLicencePlateNumber}): {CarProblemDescription}";
+        }
+    }
+}
---
+++ CarMechanic_Server/Controllers/ClientController.cs
@@ -0,0 +1,33 @@
+﻿using CarMechanic_Common.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMechanic_Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Client>> Get()
+        {
+            return Ok("Show the Client Objects");
+        }
+
+        [HttpGet]
+        public ActionResult<Client> Get(long id)
+        {
+            return Ok("Show a Client Object");
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Client client)
+        {
+            return Ok();
+        }
+    }
+}
