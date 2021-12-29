ddd0bafd4cc5ceb16ad68ed6705c7fb8b9d56e45
PC Csikusz <davidcsirak@yahoo.com>
2021-12-29 13:18:33 +0100

    Office WPF client done

---
+++ CarMechanicOffice_Client/DataProviders/CustomerOrderDataProvider.cs
@@ -0,0 +1,75 @@
+﻿using CarMechanic_Common.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMechanicOffice_Client.DataProviders
+{
+    class CustomerOrderDataProvider
+    {
+        private const string _url = "http://localhost:5000/api/customerorder";
+
+        public static IEnumerable<CustomerOrder> GetCustomerOrders()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(_url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rawData = response.Content.ReadAsStringAsync().Result;
+                    var orders = JsonConvert.DeserializeObject<IEnumerable<CustomerOrder>>(rawData);
+                    return orders;
+                }
+
+                throw new InvalidOperationException(response.StatusCode.ToString());
+            }
+        }
+
+        public static void CreateCustomerOrder(CustomerOrder order)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(order);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(_url, content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void UpdateCustomerOrder(CustomerOrder order)
+        {
+            using (var client = new HttpClient())
+            {
+                var rawData = JsonConvert.SerializeObject(order);
+                var content = new StringContent(rawData, Encoding.UTF8, "application/json");
+
+                var response = client.PutAsync($"{_url}/{order.Id}", content).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static void DeleteCustomerOrder(long id)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.DeleteAsync($"{_url}/{id}").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ CarMechanicOffice_Client/OrderWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using CarMechanic_Common.Models;
+using CarMechanicOffice_Client.DataProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarMechanicOffice_Client
+{
+    /// <summary>
+    /// Interaction logic for OrderWindow.xaml
+    /// </summary>
+    public partial class OrderWindow : Window
+    {
+        private readonly CustomerOrder _order;
+
+        public OrderWindow(CustomerOrder order)
+        {
+            InitializeComponent();
+
+            if (order != null)
+            {
+                _order = order;
+
+                FirstNameTextBox.Text = _order.FirstName; 
+                LastNameTextBox.Text = _order.LastName;
+                ModelTextBox.Text = _order.CarModel;
+                PlateNumberModelTextBox.Text = _order.CarLicencePlateNumber;
+                DescriptionModelTextBox.Text = _order.CarProblemDescription;
+
+                CreateButton.Visibility = Visibility.Collapsed;
+                UpdateButton.Visibility = Visibility.Visible;
+                DeleteButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                _order = new CustomerOrder();
+
+                CreateButton.Visibility = Visibility.Visible;
+                UpdateButton.Visibility = Visibility.Collapsed;
+                DeleteButton.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateCustomerOrder())
+            {
+                _order.FirstName = FirstNameTextBox.Text;
+                _order.LastName = LastNameTextBox.Text;
+                _order.CarModel = ModelTextBox.Text;
+                _order.CarLicencePlateNumber = PlateNumberModelTextBox.Text;
+                _order.CarProblemDescription = DescriptionModelTextBox.Text;
+                _order.WorkStatus = CarWorkStatus.Recorded;
+
+                CustomerOrderDataProvider.CreateCustomerOrder(_order);
+
+                DialogResult = true;
+                Close();
+            }
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateCustomerOrder())
+            {
+                _order.FirstName = FirstNameTextBox.Text;
+                _order.LastName = LastNameTextBox.Text;
+                _order.CarModel = ModelTextBox.Text;
+                _order.CarLicencePlateNumber = PlateNumberModelTextBox.Text;
+                _order.CarProblemDescription = DescriptionModelTextBox.Text;
+
+                CustomerOrderDataProvider.UpdateCustomerOrder(_order);
+
+                DialogResult = true;
+                Close();
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to delete?", "Question", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                CustomerOrderDataProvider.DeleteCustomerOrder(_order.Id);
+
+                DialogResult = true;
+                Close();
+            }
+        }
+
+        private bool ValidateCustomerOrder()
+        {
+            // TODO: plate number format validation and whitespace validation
+
+            if (string.IsNullOrEmpty(FirstNameTextBox.Text))
+            {
+                MessageBox.Show("First name should not be empty.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(LastNameTextBox.Text))
+            {
+                MessageBox.Show("Last name should not be empty.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(ModelTextBox.Text))
+            {
+                MessageBox.Show("Car model should not be empty.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(PlateNumberModelTextBox.Text))
+            {
+                MessageBox.Show("Licence plate number should not be empty.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(DescriptionModelTextBox.Text))
+            {
+                MessageBox.Show("Problem description should not be empty.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
